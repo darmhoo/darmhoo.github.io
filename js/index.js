71076fdfdc39484ae039d3aa5f05e57c28d8a065
Omodamola <darmhoo@hotmail.com>
2019-04-29 14:44:11 +0100

    Adjusted

--- js/index.js
+++ js/index.js
@@ -1,119 +1,98 @@
-// import idb from 'idb';
-class Main {
+// class Main {
+//     constructor() {
+//         this.currencyURL = "https://free.currencyconverterapi.com/api/v6/currencies?apiKey=a4ea424ae0960f4369ea";
+//         this.convertURL = "https://free.currencyconverterapi.com/api/v6/convert?q=";
+//     }
+//     getCurrencyList() {
+//         const topSelect = document.getElementById('dd');
+//         const bottomSelect = document.getElementById('dd2');
+//
+//
+//         fetch(this.currencyURL).then((response) => {
+//             response.json().then((myJson) => {
+//                 // console.log(myJson.results.AED.id);
+//                 for (let item in myJson.results) {
+//
+//                     const opt1 = document.createElement('option');
+//                     opt1.value = item;
+//                     opt1.innerHTML = item;
+//                     topSelect.appendChild(opt1);
+//
+//
+//                 }
+//                 for (let item in myJson.results) {
+//
+//                     const opt2 = document.createElement('option');
+//                     opt2.value = item;
+//                     opt2.innerHTML = item;
+//
+//                     bottomSelect.appendChild(opt2);
+//
+//                 }
+//             }).catch((err) => {
+//                 console.log("Can't fetch currencies: "+ err);
+//             });
+//         });
+//     }
+//
+//     getCurrencyConvert(from, dest) {
+//         const apiKey = 'a4ea424ae0960f4369ea';
+//         const url = `${this.convertURL}${from}_${dest}&compact=ultra&apiKey=${apiKey}`;
+//         fetch(url).then((response) => {
+//             return response.json();
+//         });
+//
+//     }
+//
+// }
+// const m = new Main();
+// m.getCurrencyList();
+// const button = document.getElementById('btn');
+// button.addEventListener('click', () => {
+//     const inputFirst = document.getElementById('first');
+//     const topSelect = document.getElementById('dd');
+//     const from = topSelect.value;
+//     const inputSecond = document.getElementById('second');
+//     const bottomSelect = document.getElementById('dd2');
+//     const dest = BottomSelect.value;
+//     const apiKey = 'a4ea424ae0960f4369ea';
+//     const url = `${m.convertURL}${from}_${dest}&compact=ultra&apiKey=${apiKey}`;
+//     if (topSelect.textContent !== '--select--' && bottomSelect.innerText !== '--select--'  && !isNaN(inputFirst.value())) {
+//         console.log(m.getCurrencyConvert(from, dest));
+//     } else {
+//         alert('Select any of the currencies');
+//     }
+// });
 
-    constructor() {
-        this.currencyURL = 'https://free.currencyconverterapi.com/api/v5/currencies&compact=ultra&apiKey=a4ea424ae0960f4369ea';
-        this.convertUrl = 'https://free.currencyconverterapi.com/api/v5/convert?q=&compact=ultra&apiKey=a4ea424ae0960f4369ea';
-        this.dbPromise = this.openDatabase();
-    }
-    static getData(url) {
-        let Httpreq = new XMLHttpRequest(); // a new request
-        Httpreq.open("GET", url, false);
-        Httpreq.send(null);
-        return JSON.parse(Httpreq.responseText);
-    }
-
-    openDatabase() {
-        let url = this.currencyURL;
-        const db_name = 'converter';
-        let request = indexedDB.open(db_name, 1);
-
-        request.onerror = function() {
-            return;
-        }
-        request.onupgradeneeded = function() {
-
-            let db = event.target.result;
-
-            let objectStore = db.createObjectStore("countries", { keypath: "id" });
-
-
-            objectStore.createIndex('currencyName', 'currencyName', { unique: true });
-
-
-
-
-        }
-    }
-
-
-    registerServiceWorker() {
-        if (!navigator.serviceWorker)
-            return;
-        navigator.serviceWorker.register('sw.js').then(function() {
-            console.log('registration worked again!');
-        }).catch(function() {
-            console.log('registration failed!');
-        })
-    }
-    dropDown() {
-        let dbm = this.dbPromise;
-        fetch(this.currencyURL).then((resp) => resp.json()).
-        then(function(data) {
-
-            console.log(data);
-            // dbm.then(function (db) {
-            //     if(!db) return;
-            //     let tx = db.transaction('countries', "readwrite");
-            //     let store = tx.objectStore('countries');
-            //     data.forEach(function (d) {
-            //         store.add(d);
-            //     })
-            //
-            // });
-            //     const select = document.getElementById('dd');
-            //     const select2 = document.getElementById('dd2');
-            //     for ( let key of Object.keys(data.results)){
-            //         let option = document.createElement('option');
-            //         option.text = data.results[key].id;
-            //         select.add(option);}
-            //     for ( let key of Object.keys(data.results)){
-            //         let option = document.createElement('option');
-            //         option.text = data.results[key].id;
-            //         select2.add(option);
-            // }
-        }).catch(function() {
-            console.log('Not working');
-        });
-    }
-
-
-
-    convertTask() {
-        let btn = document.getElementById('btn');
-        let url = this.convertUrl;
-        let m = Main.getData;
-
-        btn.onclick = function() {
-
-            let display = document.getElementById('second');
-            let amount = document.getElementById('first').value;
-            const from_currency = document.getElementById('dd').value;
-            const to_currency = document.getElementById('dd2').value;
-            let query = `${from_currency}_${to_currency}`;
-
-
-            fetch(`${url}${query}`).then((resp) => resp.json()).
-            then(function(data) {
-                let val = data['results'][query]['val'];
-                if (val) {
-                    let total = val * amount;
-                    total = Math.round(total * 100) / 100;
-                    display.value = total;
-                }
-            }).catch(() => console.log('No response received')
-
-            );
-
-
+const currencyURL = "https://free.currencyconverterapi.com/api/v6/currencies?apiKey=a4ea424ae0960f4369ea";
+const convertURL = "https://free.currencyconverterapi.com/api/v6/convert?q=";
+const select1 = document.getElementById('dd');
+const select2 = document.getElementById('dd2');
+function getCurrencyList() {
+    fetch(currencyURL).then((response)=>{
+        return response.json()
+    }).then((myJson)=>{
+        for (let item in myJson.results){
+            const opt1 = document.createElement('option');
+            opt1.value = item;
+            opt1.innerHTML = item;
+            select1.appendChild(opt1);
 
         }
+        for (let item in myJson.results){
+            const opt2 = document.createElement('option');
+            opt2.value = item;
+            opt2.innerHTML = item;
 
-    }
-
+            select2.appendChild(opt2);
+        }
+    })
 }
-let m = new Main();
 
-m.registerServiceWorker();
-m.dropDown();
-m.convertTask();
+getCurrencyList();
+const  btn = document.getElementById('btn');
+btn.addEventListener('click', ()=>{
+    const input = document.getElementById('first');
+    const second = document.getElementById('second');
+    second.value = input.value;
+});
